9bef458bd37216397db13ea9be617276f3d46995
EVARATE <GreeeNLambda@googlemail.com>
2020-05-20 19:28:21 +0200

    Started Rewrite.

--- commandclient.h
+++ commandclient.h
@@ -3,96 +3,26 @@
 #include "sleepy-discord/include/sleepy_discord/websocketpp_websocket.h"
 #include "sleepy-discord/include/sleepy_discord/client.h"
 #include "sleepy-discord/include/sleepy_discord/server.h"
-#include <vector>
-#include <iterator>
-#include <string>
-#include <regex>
-#include <random>
-#include <thread>
-#include <chrono>
-#include <ctime>
-#include <limits>
+#include "miscCode.cpp"
 
-typedef std::vector<std::string> stringVec;
-
-typedef struct{
-    int weekday;//1-7: Mon-Sun
-    int day;
-    int month;//1-12: Jan-Dec
-    int year;
-
-    int second;
-    int minute;
-    int hour;
-}timeObj;
-
-typedef struct{
-    std::string name = "unbekannt";
-    std::string termin1 = "unbekannt";
-    std::string termin2 = "unbekannt";
-    std::string klausur = "unbekannt";
-    std::string nachklausur = "unbekannt";
-    std::string zoomDaten = "unbekannt";
-}vorlesung;
-
-
-
-class commandClient : public SleepyDiscord::DiscordClient {
+class commandClient : public SleepyDiscord::DiscordClient{
 public:
     using SleepyDiscord::DiscordClient::DiscordClient;
+    //Session actions
     void onMessage(SleepyDiscord::Message message);
-    void onError(SleepyDiscord::ErrorCode errorCode, const std::string errorMessage);
     void onReady(std::string* jsonMessage);
     void onDisconnect();
     void onResume();
 
-    //Access:
-    SleepyDiscord::Channel              channelByName(std::string channelName);
-    SleepyDiscord::User                 userByName(std::string userName);
-    SleepyDiscord::User                 userByNick(std::string userNick);
-    std::vector<SleepyDiscord::User>    usersInChannel(std::string channelID);
-    std::vector<SleepyDiscord::User>    usersWithRole(std::string roleName);
-    bool                                userHasRole(std::string userID, std::string roleName);
-    std::string getRuleMsg();
-
-    //Actions
-    void toConsoleLog(const std::string &text);
-    void setupData();
-    void updateHelpMsg();
-    timeObj getCurrentTime();
+    //Commands
+    void execTextCommand(stringVec& command, SleepyDiscord::Message& message);
 
     //Other
-    void addLecture(const std::string& name,
-                    const std::string& termin1,
-                    const std::string& termin2,
-                    const std::string& klausur,
-                    const std::string& nachklausur,
-                    const std::string& zoomDaten);
-    //Commands
-    void sendRulesMsg(SleepyDiscord::Message message);
-    void sendHelpMsg(SleepyDiscord::Message message);
-    void setPrefix(std::string newPrefix);
-    void sendRandom(SleepyDiscord::Message message, std::string rawOptions);
-    void sendLectureInfo(SleepyDiscord::Message message, std::string name);
-    void sendDirMessage(const std::string& userName, const std::string& text);
-
+    void loadTextCommands();
 
 private:
-    //Vorlesungen:
-    std::vector<vorlesung> vorlesungen;
-
-    //Bot options:
+    //Data:
     std::string prefix = "t/";
-    std::string helpMessage = "";
-
-    //Server:
-    const std::string serverID = "541324032042205196";
-    //Important messages:
-    std::string ruleMessageID = "702501801047621681";
-    std::string helpMessageID = "702969362679857283";
-
-    bool isConnected = false;
-    timeObj lastDisconnect;
-};
+};  std::vector<textCommand> textCommands;
 
 #endif // COMMANDCLIENT_H
---
+++ miscCode.cpp
@@ -0,0 +1,95 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+//=====CONSTANTS=====
+const std::string configPath = "~/.config/discord_bot/";
+
+//=====TYPEDEFS=====
+typedef std::vector<std::string> stringVec;
+
+typedef struct{
+    int weekday;//1-7: Mon-Sun
+    int day;
+    int month;//1-12: Jan-Dec
+    int year;
+
+    int second;
+    int minute;
+    int hour;
+}timeObj;
+
+typedef struct{
+    std::string name;
+    std::string value;
+}property;
+
+typedef struct{
+    stringVec commands;//Multiple commands can trigger this
+    std::vector<property> properties;
+}textCommand;
+
+//=====FUNCTIONS=====
+std::string getToken(){
+    std::string token;
+    std::ifstream ifile;
+    ifile.open(configPath + "token.txt");
+    if(!ifile.is_open()){
+        //Enter token manually:
+        std::cout << "Couldn't read 'token.txt'.\n";
+        bool invalidToken;
+        do{
+            std::cout << "Enter token: ";
+
+            std::cin >> token;
+            if(token.length() == 59){
+                invalidToken = false;
+            }else{
+                std::cout << "Invalid token!\n";
+                invalidToken = true;
+            }
+        }while(invalidToken);
+        //Save token to file:
+        std::ofstream ofile;
+        ofile.open(configPath + "token.txt");
+        if(ofile.is_open()){
+            ofile << token;
+            ofile.close();
+            return token;
+        }else{
+            std::cout << "Couldn't save token to token.txt\n";
+            return token;
+        }
+    }else{
+        ifile >> token;
+        if(token.length() == 59){
+            return token;
+        }else{
+            std::cout << "Invalid token!\n";
+            return "";
+        }
+    }
+
+}
+std::string toLowerCase(std::string str){
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c){return std::tolower(c);});
+    return str;
+}
+stringVec returnMatches(std::string str, std::regex reg){
+    stringVec sVec;
+    std::sregex_iterator currentMatch(str.begin(), str.end(), reg);
+    std::sregex_iterator lastMatch;
+    while(currentMatch != lastMatch){
+        std::smatch match = *currentMatch;
+        sVec.push_back(match.str());
+        currentMatch++;
+    }
+    return sVec;
+}
+stringVec toWords(std::string str){
+    std::regex reg("[^ ]+");
+    return returnMatches(str, reg);
+}
