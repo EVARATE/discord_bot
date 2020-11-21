2544a28703ced7614ba8b18b1c16f05a4876964a
EVARATE <GreeeNLambda@googlemail.com>
2020-11-22 00:22:25 +0100

    Implemented basic bot

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hi")
+import discord
+
+
+class bot_client(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message received')
+
+
+token = ""
+client = bot_client()
+client.run(token)
