6907f2f682c0b7153362d1683658ee8a37538cb1
EVARATE <GreeeNLambda@googlemail.com>
2020-11-22 01:03:38 +0100

    Implemented basic bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-
+import re # regex
+import misc_functions as misc
 
 class bot_client(discord.Client):
     async def on_ready(self):
@@ -8,7 +9,22 @@ class bot_client(discord.Client):
     async def on_message(self, message):
         print('Message received')
 
+    def get_config(self):
+        configFile = open('config.txt', 'r')
+        rawText = configFile.read()
 
-token = ""
+        # Find parameters via regex:    .+ = '.+'
+        params = re.findall('.+ = \'.+\'', rawText)
+
+        # Assign found parameters to variables:
+        lineCount = 0
+        while lineCount < len(params):
+
+
+
+
+    # Parameters
+    token = ''
+
 client = bot_client()
-client.run(token)
+client.run(bot_client.token)
---
+++ misc_functions.py
@@ -0,0 +1,3 @@
+'''
+This file contains functions the bot needs but that are not part of the bot_client class.
+'''
