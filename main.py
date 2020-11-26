69311802e5b029f6a377aab905a0c0324cdc6928
EVARATE <GreeeNLambda@googlemail.com>
2020-11-26 19:51:47 +0100

    New command: /help, Bot now displays activity message

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+'''
+If you don't know what the fuck is going on here then I humbly suggest you read the documentation:
+https://discordpy.readthedocs.io/en/latest/index.html#
+
+'''
+
+
 import discord
+from discord.ext import commands
 import configparser
 import random
+
 
 # Modify class:
 class bot_client(discord.Client):
@@ -8,21 +17,22 @@ class bot_client(discord.Client):
         # Run this function once when the bot starts for the first time
         self.get_config()
 
+
     def get_config(self):
         configFile = configparser.ConfigParser()
         configFile.read('config.txt')
         self.token = configFile['BASE']['token']
         self.prefix = configFile['BASE']['prefix']
-        self.ruleChannelID = configFile['RULES']['channelID']
-        self.ruleMessageID = configFile['RULES']['messageID']
+        self.ruleChannelID = int(configFile['RULES']['channelID'])
+        self.ruleMessageID = int(configFile['RULES']['messageID'])
 
 
 
     # Parameters:
     token = ''
     prefix = ''
-    ruleChannelID = ''
-    ruleMessageID = ''
+    ruleChannelID = -1
+    ruleMessageID = -1
 
 
 # Callbacks:
@@ -31,6 +41,8 @@ client = bot_client()
 @client.event
 async def on_ready():
     print('{0.user} is now online.'.format(client))
+    game = discord.Game("mit deinen Nerven")
+    await client.change_presence(status=discord.Status.online, activity = game)
 
 @client.event
 async def on_message(message):
@@ -52,10 +64,11 @@ async def on_message(message):
             await message.channel.send('I am afraid I can\'t do that {0.author.name}.'.format(message))
             return
 
-        await message.channel.send('Command detected')
-
-
-
+        # RULES
+        if message.content.startswith(client.prefix + "help"):
+            ruleChannel: discord.abc.GuildChannel = client.get_channel(client.ruleChannelID)
+            ruleMsg = await ruleChannel.fetch_message(client.ruleMessageID)
+            await message.channel.send(ruleMsg.content)
 
 
 client.startup()
