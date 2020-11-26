80ab448faf946f5c7aad91f845d00c9153f75391
EVARATE <GreeeNLambda@googlemail.com>
2020-11-26 14:58:36 +0100

    Restructured everything to be closer to the preferred way of the holy documentation.

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import discord
 import configparser
-from discord.ext import commands
-import misc_functions as misc
 
+# Modify class:
 class bot_client(discord.Client):
     def startup(self):
         # Run this function once when the bot starts for the first time
         self.get_config()
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message received')
 
     def get_config(self):
         configFile = configparser.ConfigParser()
         configFile.read('config.txt')
-        self.token = configFile['DEFAULT']['token']
+        self.token = configFile['BASE']['token']
+        self.prefix = configFile['BASE']['prefix']
         self.ruleChannelID = configFile['RULES']['channelID']
         self.ruleMessageID = configFile['RULES']['messageID']
 
@@ -24,9 +19,33 @@ class bot_client(discord.Client):
 
     # Parameters:
     token = ''
+    prefix = ''
     ruleChannelID = ''
     ruleMessageID = ''
 
+
+# Callbacks:
 client = bot_client()
-client.get_config()
+
+@client.event
+async def on_ready():
+    print('{0.user} is now online.'.format(client))
+
+@client.event
+async def on_message(message):
+    # Bot doesn't answer itself:
+    if message.author == client.user:
+        return
+
+    # === INTERPRET CHAT ===
+
+    # look for commands:
+    if message.content.startswith(client.prefix):
+        await message.channel.send('Command detected')
+
+
+
+
+
+client.startup()
 client.run(client.token)
--- misc_functions.py
+++ misc_functions.py
@@ -1,3 +1,14 @@
 '''
 This file contains functions the bot needs but that are not part of the bot_client class.
 '''
+
+import configparser
+
+def load_config():
+    configFile = configparser.ConfigParser()
+    configFile.read('config.txt')
+
+
+    self.token = configFile['BASE']['token']
+    self.ruleChannelID = configFile['RULES']['channelID']
+    self.ruleMessageID = configFile['RULES']['messageID']
