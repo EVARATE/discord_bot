12408b34c81184c936a30194350eea9cab821273
EVARATE <GreeeNLambda@googlemail.com>
2020-11-27 09:50:05 +0100

    Removed redundant code from misc_functions.py

--- misc_functions.py
+++ misc_functions.py
@@ -3,13 +3,3 @@ This file contains functions the bot needs but that are not part of the bot_clie
 This makes everything more organized.
 '''
 
-import configparser
-
-def load_config():
-    configFile = configparser.ConfigParser()
-    configFile.read('config.txt')
-
-
-    self.token = configFile['BASE']['token']
-    self.ruleChannelID = configFile['RULES']['channelID']
-    self.ruleMessageID = configFile['RULES']['messageID']
